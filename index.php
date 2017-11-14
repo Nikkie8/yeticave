4a519caf0b086d4e3128087435120805bb7b5dcf
Nikkie8 <nikkie223shin@gmail.com>
2017-11-14 19:00:48 +0300

    Список категорий и лотов

--- index.php
+++ index.php
@@ -20,6 +20,54 @@ $time_till_midnight = $tomorrow - $now;
 
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
 $lot_time_remaining = gmdate("H:i", $time_till_midnight);
+
+$categories = [
+    "Доски и лыжи",
+    "Крепления",
+    "Ботинки",
+    "Одежда",
+    "Инструменты",
+    "Разное"
+];
+
+$items = [
+    [
+        "title" => "2014 Rossignol District Snowboard",
+        "category" => "Доски и лыжи",
+        "price" => 10999,
+        "image" => "img/lot-1.jpg"
+    ],
+    [
+        "title" => "DC Ply Mens 2016/2017 Snowboard",
+        "category" => "Доски и лыжи",
+        "price" => 159999,
+        "image" => "img/lot-2.jpg"
+    ],
+    [
+        "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
+        "category" => "Крепления",
+        "price" => 8000,
+        "image" => "img/lot-3.jpg"
+    ],
+    [
+        "title" => "Ботинки для сноуборда DC Mutiny Charocal",
+        "category" => "Ботинки",
+        "price" => 10999,
+        "image" => "img/lot-4.jpg"
+    ],
+    [
+        "title" => "Куртка для сноуборда DC Mutiny Charocal",
+        "category" => "Одежда",
+        "price" => 7500,
+        "image" => "img/lot-5.jpg"
+    ],
+    [
+        "title" => "Маска Oakley Canopy",
+        "category" => "Разное",
+        "price" => 5400,
+        "image" => "img/lot-6.jpg"
+    ]
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -44,7 +92,6 @@ $lot_time_remaining = gmdate("H:i", $time_till_midnight);
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-            <!--true-->
             <?php if ($is_auth): ?>
             <div class="user-menu__image">
                 <img src="<?=$user_avatar; ?>" width="40" height="40" alt="Пользователь">
@@ -52,7 +99,6 @@ $lot_time_remaining = gmdate("H:i", $time_till_midnight);
             <div class="user-menu__logged">
                 <p><?=$user_name; ?></p>
             </div>
-            <!--false-->
             <?php else: ?>
             <ul class="user-menu__list">
                 <li class="user-menu__item">
@@ -72,24 +118,13 @@ $lot_time_remaining = gmdate("H:i", $time_till_midnight);
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="all-lots.html">Доски и лыжи</a>
-            </li>
-            <li class="promo__item promo__item--attachment">
-                <a class="promo__link" href="all-lots.html">Крепления</a>
-            </li>
-            <li class="promo__item promo__item--boots">
-                <a class="promo__link" href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="promo__item promo__item--clothing">
-                <a class="promo__link" href="all-lots.html">Одежда</a>
-            </li>
-            <li class="promo__item promo__item--tools">
-                <a class="promo__link" href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="promo__item promo__item--other">
-                <a class="promo__link" href="all-lots.html">Разное</a>
-            </li>
+            <?php  $cat_index = 0; ?>
+            <?php while ($cat_index < count($categories)) { ?>
+                <li class="promo__item promo__item--boards">
+                    <a class="promo__link" href="all-lots.html"><?=$categories[$cat_index]; ?></a>
+                </li>
+                <?php $cat_index++; ?>
+            <?php } ?>
         </ul>
     </section>
     <section class="lots">
@@ -97,24 +132,26 @@ $lot_time_remaining = gmdate("H:i", $time_till_midnight);
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            <?=$lot_time_remaining;?>
+            <?php foreach ($items as $key => $item): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$item['image']; ?>" width="350" height="260" alt="Сноуборд">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$item['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$item['title']; ?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$item['price']; ?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                <?=$lot_time_remaining;?>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
