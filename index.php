2fdc7f0364e4d4a96c9a2ec8df59d262fedab480
Nikkie8 <nikkie223shin@gmail.com>
2017-11-17 21:53:14 +0300

    Refactoring

--- index.php
+++ index.php
@@ -3,72 +3,72 @@ require_once('functions.php');
 
 $categories = [
     [
-        "modifier" => "boards",
-        "category" => "Доски и лыжи"
+        'modifier' => 'boards',
+        'category' => 'Доски и лыжи'
     ],
     [
-        "modifier" => "attachment",
-        "category" => "Крепления"
+        'modifier' => 'attachment',
+        'category' => 'Крепления'
     ],
     [
-        "modifier" => "boots",
-        "category" => "Ботинки"
+        'modifier' => 'boots',
+        'category' => 'Ботинки'
     ],
     [
-        "modifier" => "clothing",
-        "category" => "Одежда"
+        'modifier' => 'clothing',
+        'category' => 'Одежда'
     ],
     [
-        "modifier" => "tools",
-        "category" => "Инструменты"
+        'modifier' => 'tools',
+        'category' => 'Инструменты'
     ],
     [
-        "modifier" => "other",
-        "category" => "Разное"
+        'modifier' => 'other',
+        'category' => 'Разное'
     ]
 ];
 
 $items = [
     [
-        "title" => "2014 Rossignol District Snowboard",
-        "category" => "Доски и лыжи",
-        "price" => 10999,
-        "image" => "img/lot-1.jpg"
+        'title' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 10999,
+        'image' => 'img/lot-1.jpg'
     ],
     [
-        "title" => "DC Ply Mens 2016/2017 Snowboard",
-        "category" => "Доски и лыжи",
-        "price" => 159999,
-        "image" => "img/lot-2.jpg"
+        'title' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 159999,
+        'image' => 'img/lot-2.jpg'
     ],
     [
-        "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
-        "category" => "Крепления",
-        "price" => 8000,
-        "image" => "img/lot-3.jpg"
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => 8000,
+        'image' => 'img/lot-3.jpg'
     ],
     [
-        "title" => "Ботинки для сноуборда DC Mutiny Charocal",
-        "category" => "Ботинки",
-        "price" => 10999,
-        "image" => "img/lot-4.jpg"
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => 10999,
+        'image' => 'img/lot-4.jpg'
     ],
     [
-        "title" => "Куртка для сноуборда DC Mutiny Charocal",
-        "category" => "Одежда",
-        "price" => 7500,
-        "image" => "img/lot-5.jpg"
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => 7500,
+        'image' => 'img/lot-5.jpg'
     ],
     [
-        "title" => "Маска Oakley Canopy",
-        "category" => "Разное",
-        "price" => 5400,
-        "image" => "img/lot-6.jpg"
+        'title' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => 5400,
+        'image' => 'img/lot-6.jpg'
     ]
 ];
 
 // записать в эту переменную оставшееся время в этом формате (ЧЧ:ММ)
-$lot_time_remaining = "00:00";
+$lot_time_remaining = '00:00';
 
 // временная метка для полночи следующего дня
 $tomorrow = strtotime('tomorrow midnight');
@@ -79,7 +79,7 @@ $now = strtotime('now');
 $time_till_midnight = $tomorrow - $now;
 
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
-$lot_time_remaining = gmdate("H:i", $time_till_midnight);
+$lot_time_remaining = gmdate('H:i', $time_till_midnight);
 
 $page_content = render_template('templates/index.php', [
     'categories' => $categories,
