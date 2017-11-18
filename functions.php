ea5dd8eaf913d6fdea84d2618f14fd0663ee3138
Nikkie8 <nikkie223shin@gmail.com>
2017-11-18 12:31:12 +0300

    Refactoring

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 function render_template($template_url = '', $data = []) {
+    $result = '';
+
     if (file_exists($template_url)) {
         ob_start();
         extract($data);
         require_once($template_url);
         $result = ob_get_clean();
-    } else {
-        $result = '';
     }
 
     return $result;
