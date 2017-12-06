54da3dc9d9d60fb8651d34da23020dde8cfc8bda
Nikkie8 <nikkie223shin@gmail.com>
2017-12-06 18:34:30 +0300

    Добавила создание юзеров

--- queries.sql
+++ queries.sql
@@ -4,3 +4,19 @@ INSERT INTO categories SET name = 'Ботинки', modifier = 'boots';
 INSERT INTO categories SET name = 'Одежда', modifier = 'clothing';
 INSERT INTO categories SET name = 'Инструменты', modifier = 'tools';
 INSERT INTO categories SET name = 'Разное', modifier = 'other';
+
+INSERT INTO users SET
+  email = 'ignat.v@gmail.com',
+  name = 'Игнат',
+  password = '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka',
+  avatar = 'img/avatar.jpg';
+INSERT INTO users SET
+  email = 'kitty_93@li.ru',
+  name = 'Леночка',
+  password = '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa',
+  avatar = 'img/user.jpg';
+INSERT INTO users SET
+  email = 'warrior07@mail.ru',
+  name = 'Руслан',
+  password = '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW',
+  avatar = 'img/user.jpg';
