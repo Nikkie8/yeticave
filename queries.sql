eeea0114fa0d4f1e3d273669651efbc2d9498f7d
Nikkie8 <nikkie223shin@gmail.com>
2017-12-06 18:28:29 +0300

    Добавила в категории модификатор, скрипт создания записей

---
+++ queries.sql
@@ -0,0 +1,6 @@
+INSERT INTO categories SET name = 'Доски и лыжи', modifier = 'boards';
+INSERT INTO categories SET name = 'Крепления', modifier = 'attachment';
+INSERT INTO categories SET name = 'Ботинки', modifier = 'boots';
+INSERT INTO categories SET name = 'Одежда', modifier = 'clothing';
+INSERT INTO categories SET name = 'Инструменты', modifier = 'tools';
+INSERT INTO categories SET name = 'Разное', modifier = 'other';
